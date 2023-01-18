9c0f58953b9235f41eeff6cb5af2bfcd5fbcd0aa
andrabuzila <72390634+andrabuzila@users.noreply.github.com>
2023-01-18 19:28:31 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangochat',
+        'USER': 'postgres',
+        'PASSWORD': 'andra',
+        'HOST': 'db-container',
+        'PORT': '5432',
     }
 }
 
